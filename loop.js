e8f7741bd11f27ed091250cd87e6a22ba5551e83
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2024-04-19 10:39:07 +0530

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,102 @@
+// for while do while
+
+// for(let i = 1;i <= 10;i+=2){
+//   console.log(i)
+// }
+
+// when loops starts , the Iterator let i = 1; (gets declared and intiliazed)
+// then the iterator goes on to the condition where it checks whether the condition is true or not
+// If it gets true , the inside code will run or execute
+// then the iterator will get incremented by 1;
+// then again it will repeat step 2 and step 3
+
+// Do while
+// let i = 1;
+
+// do{
+//   console.log(i)
+//   i++
+// }while(i<=10)
+
+// while loops
+
+// let i = 0;
+// while(i <= 10){
+//   console.log(i)
+//   i++;
+// }
+
+
+// function Table(num,num1){ //num which I passed while creating the function , this will be called a parameter
+//   let i = 0;
+// while(i <= 10){
+//   console.log(num + ' * ' + i + ' = ',num*i)
+//   // console.log(num1 * i)
+//   i++;
+// }
+// }
+
+// Table(8)  //2 passed over here is an argument , while calling a function , value which we pass , is called argument.
+
+// for(let i = 10; i >= 1;i--){
+//   console.log(i)
+// }
+
+// search even and odd numbers between 1 to 100
+// let evenCount = 0;
+// // let oddCount = 0;
+// for(let i = 1;i <= 10;i++){
+//   if(i%2==0){
+//     evenCount+=i;
+//   }
+// }
+
+// console.log("Even:",evenCount )
+
+// Factorial
+
+// function factorial(num){
+//   let product = 1;
+//   for(let i = 1;i <= num;i++){
+//     product *= i;
+//   }
+//   return product;
+// }
+
+// console.log("Factorial:",factorial(10))
+
+// NITIN
+// function isPalindrome(str){
+//   let j = str.length - 1;
+//   // console.log(str[j])
+//   for(let i = 0; i <= str.length;){
+//   if(str[i] === str[j] && i !== j){
+//     // console.log("Matched",str[i])
+//     i++;
+//     j--
+//   }
+//   else if(i == j && str[i]===str[j]){
+//     // console.log(i,'>>>')
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+//   // return false;
+//   }
+// }
+
+
+// console.log(isPalindrome("karan"))
+
+
+
+
+
+
+
+
+
+
+
+
